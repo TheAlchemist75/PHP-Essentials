69ef7a0b038b0f815ef3c514083fce39157e5aa7
TheAlchemist75 <shubhammanur0075@gmail.com>
2024-03-23 15:40:16 +0100

    Added dash.php

---
+++ EMS/dash.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>EMS - Dashboard</title>
+</head>
+
+<body>
+    <section>
+        <div class="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 md:px-8">
+            <div class="space-y-5 max-w-4xl mx-auto text-center">
+                <h1 class="text-sm text-indigo-600 font-medium">
+                    Welcome, Username
+                </h1>
+                <h2 class="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
+                    Manage your employees faster with <span
+                        class="text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]"
+                        style="background-image: linear-gradient(to right, #4F46E5, #E114E5);"> Employee management
+                        system</span>
+                </h2>
+                <p class="max-w-2xl mx-auto">
+                    Displays how many employees are present in your company, modify or update employees details and
+                    delete entities
+                </p>
+                <div class="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
+                    <a href="javascript:void(0)"
+                        class="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
+                        View employees
+                    </a>
+                    <a href="javascript:void(0)"
+                        class="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                        Modify details
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
